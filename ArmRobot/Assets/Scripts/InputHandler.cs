5efaf4356d671d1b602ce7bb4956aa22793a3a1d
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-26 14:36:46 +0200

    Every Joint Moving
    Not an elegant solution

--- ArmRobot/Assets/Scripts/InputHandler.cs
+++ ArmRobot/Assets/Scripts/InputHandler.cs
@@ -13,5 +13,36 @@ public class InputHandler : MonoBehaviour
         if (Input.GetAxis("Base") > 0) {
             robotController.RotateJoint(JointNeeded.Base, RotationDirection.Positive);
         }
+        if (Input.GetAxis("Shoulder") < 0) {
+            robotController.RotateJoint(JointNeeded.Shoulder, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Shoulder") > 0) {
+            robotController.RotateJoint(JointNeeded.Shoulder, RotationDirection.Positive);
+        }
+        if (Input.GetAxis("Elbow") < 0) {
+            robotController.RotateJoint(JointNeeded.Elbow, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Elbow") > 0) {
+            robotController.RotateJoint(JointNeeded.Elbow, RotationDirection.Positive);
+        }
+        if (Input.GetAxis("Wrist1") < 0) {
+            robotController.RotateJoint(JointNeeded.Wrist1, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Wrist1") > 0) {
+            robotController.RotateJoint(JointNeeded.Wrist1, RotationDirection.Positive);
+        }
+        if (Input.GetAxis("Wrist2") < 0) {
+            robotController.RotateJoint(JointNeeded.Wrist2, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Wrist2") > 0) {
+            robotController.RotateJoint(JointNeeded.Wrist2, RotationDirection.Positive);
+        }
+        if (Input.GetAxis("Wrist3") < 0) {
+            robotController.RotateJoint(JointNeeded.Wrist3, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Wrist3") > 0) {
+            robotController.RotateJoint(JointNeeded.Wrist3, RotationDirection.Positive);
+        }
+
     }
 }   
