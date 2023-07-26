eb01ebfade7f3b85c6d7171c939532cb212ea057
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-26 14:30:26 +0200

    UserInput Handler

--- ArmRobot/Assets/Scripts/InputHandler.cs
+++ ArmRobot/Assets/Scripts/InputHandler.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    [SerializeField] private RobotController robotController;
 
-}
+    private void Update() {
+        if (Input.GetAxis("Base") < 0) {
+            robotController.RotateJoint(JointNeeded.Base, RotationDirection.Negative);
+        }
+        if (Input.GetAxis("Base") > 0) {
+            robotController.RotateJoint(JointNeeded.Base, RotationDirection.Positive);
+        }
+    }
+}   
--- ArmRobot/Assets/Scripts/RobotController.cs
+++ ArmRobot/Assets/Scripts/RobotController.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum JointNeeded {   
+    Base = 0, 
+    Shoulder = 1, 
+    Elbow = 2,
+    Wrist1 = 3,
+    Wrist2 = 4,
+    Wrist3 = 5,
+};
+
 public class RobotController : MonoBehaviour
 {
     [SerializeField] private List<JointController> joints = new List<JointController>();
+    [SerializeField] private GameObject InputSystem;
+
 
     void Update() {
-
-        if (Input.GetKey(KeyCode.W)) {
-            for (int i = 0; i < joints.Count; i++) {
-                joints[i].SetRotation(RotationDirection.Positive);
-            }
-        }
+        
     }
+
+    public void RotateJoint(JointNeeded needed, RotationDirection rotation) {
+
+        joints[(int)needed].SetRotation(rotation);
+    }
+
+
 }
